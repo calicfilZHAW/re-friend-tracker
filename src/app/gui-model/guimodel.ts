f86b7a376f8c1af87a0ebfcf1385fe3343cad3b6
calicfilZHAW <calicfil@students.zhaw.ch>
2018-05-03 15:21:25 +0200

    updated

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -27,6 +27,13 @@ export class GuiModel {
                             "width": 1,
                             "required": true
                         },
+						{
+							"id": "nickname",
+							"type": "text",
+							"name": "Nickname",
+							"width": 2,
+							"required": true
+							},
                         {
                             "id":   "location",
                             "type": "autocomplete",
@@ -60,8 +67,17 @@ export class GuiModel {
                         {
                             "type": "okButton",
                             "name": "Ok"
-                        }
+                        },
+						{
+							"id": "group",
+							"type": "autocomplete",
+							"name": "Group",
+							"url": "/group",
+							"form": "GroupForm",
+							"width": 2
+						},
                     ]
+					
                 },
                 {
                     "id": "LocationForm",
@@ -94,6 +110,13 @@ export class GuiModel {
                     "title": "Activity",
                     "url": "/friend/:friendKey/activity",
                     "formFieldList": [
+						{
+                            "id": "name",
+                            "type": "text",
+                            "name": "ActivityName",
+                            "width": 2,
+                            "required": true
+                        },
                         {
                             "id":   "activity",
                             "type": "autocomplete",
@@ -118,6 +141,32 @@ export class GuiModel {
                         }
                     ]
                 },
+				{
+					"id": "GroupForm",
+					"title": "Group",
+					"url": "/group",
+					"formFieldList": [
+						{
+							 "id": "name",
+							 "type": "text",
+							 "name": "GroupName",
+							 "width": 2,
+							 "required": true
+						},
+						{
+							 "type": "deleteButton",
+							 "name": "Delete"
+						},
+						{
+							 "type": "cancelButton",
+							 "name": "Cancel"
+						},
+						{
+							 "type": "okButton",
+							 "name": "Ok"
+						}
+					]
+				},
             ],
             "pageList": [
                 {
@@ -137,9 +186,48 @@ export class GuiModel {
                             "icon": "fa-cubes",
                             "color": "yellow",
                             "page": "locationspage",
-                        },
+                        },{
+							"type": "button",
+							"name": "Groups",
+							"icon": "fa-weixin",
+							"color": "wisteria",
+							"page": "groupspage",
+							},
+						{
+							"type": "button",
+							"name": "Activity",
+							"icon": "fa-weixin",
+							"color": "red",
+							"page": "groupspage",
+							},
                     ]
                 },
+					{"id": "groupspage",
+					"elementList": [
+						{
+							"type": "backbutton",
+						},
+						{
+							"type": "newButton",
+							"name": "NewGroup",
+							"icon": "fa-weixin",
+							"color": "green",
+							"form": {
+								"form": "GroupForm"
+							}
+						},
+						{	"type": "list",
+							"icon": "fa-weixin",
+							"color": "wisteria",
+							"search": true,
+							"url": "/group",
+							"form": {
+							"form": "GroupForm"
+							}
+						},
+
+					]
+				},
                 {
                     "id": "friendspage",
                     "elementList": [
@@ -193,6 +281,25 @@ export class GuiModel {
                                 "form": "LocationForm"
                             }
                         },
+                    ]
+                },
+				{
+                    "id": "aktivitypage",
+                    "elementList": [
+                        {
+                            "type": "backbutton",
+                        },
+                        {
+                            "type": "newButton",
+                            "name": "NewLocation",
+                            "icon": "fa-home",
+                            "color": "green",
+                            "width": 2,
+                            "form": {
+                                "form": "LocationForm"
+                            }
+                        },
+                        
                     ]
                 },
             ]
